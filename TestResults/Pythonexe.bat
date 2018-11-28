5a49add75092e3b18e56a65926f90fa850cb0e73
Nall11G <nall11sf@gmail.com>
2018-11-28 20:19:54 +0530

    Add files via upload

---
+++ TestResults/Pythonexe.bat
@@ -0,0 +1,3 @@
+robot --include Smoke E:\Robot_Framework\Salesforce_Automation-Demo\TestCases\Reliance_Regression_TestSuite.robot & python E:\Robot_Framework\Salesforce_Automation-Demo\TestResults\robotmetrics.py
+
+
