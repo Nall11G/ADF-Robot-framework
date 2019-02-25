279292464e924076b069c180b9edcbb6925cbc6a
Nall11G <nall11sf@gmail.com>
2019-02-25 13:29:15 +0530

    Update Parallel.bat

--- TestResults/Parallel.bat
+++ TestResults/Parallel.bat
@@ -1,2 +1,2 @@
-pabot --variable  --BROWSER:${BROWSER}  --include Demo ADF-Robot-framework/TestCases/Reliance_Regression_TestSuite.robot & python ADF-Robot-framework/TestResults/robotmetrics.py
+pabot --variable  --BROWSER:${BROWSER}  --include Demo C:\Users\Mstemp120\.jenkins\workspace\ADF_Parallel Execution\TestCases\Reliance_Regression_TestSuite.robot & python ADF-Robot-framework/TestResults/robotmetrics.py
       
