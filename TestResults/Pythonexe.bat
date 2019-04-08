55f648d2b2dcb6bd409648c00230c292f2bfc233
Nall11G <nall11sf@gmail.com>
2019-04-08 18:42:30 +0530

    Update Pythonexe.bat

--- TestResults/Pythonexe.bat
+++ TestResults/Pythonexe.bat
@@ -1,1 +1,1 @@
-robot --listener allure_robotframework;E:\\Robot_Framework\\AllureReport --include Demo TestCases\Reliance_Regression_TestSuite.robot
+robot --listener allure_robotframework;C:\\Users\\Mstemp120\\.jenkins\\workspace\\ADF_Automation_Demo\\allure-report --include Demo TestCases\Reliance_Regression_TestSuite.robot
